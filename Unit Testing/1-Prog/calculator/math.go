4ea75658699814936cbfaa12358e6a42ef1ee8b8
vishaldeoprasad <vishaldeoprasad@yahoo.com>
2023-10-31 18:15:56 +0530

    unit testing example

---
+++ Unit Testing/1-Prog/calculator/math.go
@@ -0,0 +1,14 @@
+package calculator
+
+// Add takes two integers and returns their sum.
+func Addition(a, b int) int {
+	return a + b
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiplication(a, b int) int {
+	return a * b
+}
