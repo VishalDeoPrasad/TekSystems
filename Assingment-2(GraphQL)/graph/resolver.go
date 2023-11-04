3124fd8f3cb0b9410b7936d1bc2b8f5760259f14
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-11-04 21:19:08 +0530

    resolver

---
+++ Assingment-2(GraphQL)/graph/resolver.go
@@ -0,0 +1,10 @@
+package graph
+
+import "golang/service"
+
+// This file will not be regenerated automatically.
+//go:generate go run github.com/99designs/gqlgen
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct{
+	 Svc service.UserService }
